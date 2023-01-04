b97b0a96aedec28361d93ff34896bcd91c93d0a1
yuto ichinose <yuto.0526.sano@gmail.com>
2023-01-05 00:35:18 +0900

    選択肢で詰まった

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+import wordData from "./data.json";
+
+interface wordProps {
+  eng: string;
+  jap: string;
+}
+interface optionWordProps<T> {
+  0: T;
+  1: T;
+  2: T;
+  3: T;
+}
 
 function App() {
+  console.log(wordData);
+  const [currentWord, setCurrentWord] = useState<wordProps>();
+  const [optionWord, setOptionWord] = useState<
+    // optionWordProps<wordProps> | undefined
+    wordProps[]
+  >();
+
+  const constraintsRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const word = wordData[Math.floor(Math.random() * wordData.length)];
+    setCurrentWord(word);
+    const options: wordProps[] = [];
+    options.push(word);
+    console.log(options.map((item) => item));
+
+    for (let i = 0; i < 3; i++) {
+      // const filterWord = wordData.filter(item => options.find(val => val.eng === item.eng))
+      const filterWord = wordData.filter((item) => word.eng !== item.eng);
+      options.push(filterWord[Math.floor(Math.random() * filterWord.length)]);
+    }
+    console.log(options);
+    setOptionWord(options);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <motion.div
+      className="App"
+      ref={constraintsRef}
+      style={{
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <Box className="wordContainer">
+        <Box className="wordPanel">{currentWord?.eng}</Box>
+        <Box className="wordDragArea">Drag Word</Box>
+      </Box>
+    </motion.div>
   );
 }
 
